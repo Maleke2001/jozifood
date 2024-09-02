832ee27ec8beacba7064bda2b1714178ec1db2a6
Maleke2001 <motshabielizabeth1@gmail.com>
2024-09-02 12:24:07 +0200

    tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import img1 from '../assets/image1.jpg'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <div className='h-screen '> 
+      <div className='flex justify-around'>
+       <img src={img1} className=''/>
+       <div className=''>
+    
+    </div>
+        <nav>
+          <a>Home</a>  
+          <a>Menu</a>
+          <a>Abous</a>
+          <a>Contact</a>
+
+        </nav>
+
+
+       </div>
+    </div>
+   
   )
 }
